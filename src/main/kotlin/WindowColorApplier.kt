26f4fe23a2979b779272cd774c601921a1f0abf8
Alex Blake-Goudemond <alex@psybergate.co.za>
2026-04-08 22:46:32 +0200

    ChatGPT: Set the coloured panel to be the Right Hand Side of the window

--- src/main/kotlin/WindowColorApplier.kt
+++ src/main/kotlin/WindowColorApplier.kt
@@ -16,15 +16,11 @@ object WindowColorApplier {
 
             val color = generateColor(project.name)
 
-            val glassPane = root.glassPane as? javax.swing.JComponent ?: return@invokeLater
-            glassPane.layout = null
-
             val panel = JPanel()
             panel.background = color
-            panel.setBounds(0, 0, frame.width, 10)
+            panel.preferredSize = java.awt.Dimension(20, 0) // thin vertical strip
 
-            glassPane.add(panel)
-            glassPane.isVisible = true
+            root.contentPane.add(panel, java.awt.BorderLayout.EAST)
         }
     }
 
@@ -33,6 +29,6 @@ object WindowColorApplier {
         val r = (hash shr 16) and 0xFF
         val g = (hash shr 8) and 0xFF
         val b = hash and 0xFF
-        return Color(r, g, b)
+        return Color(r, g, b, 180)
     }
 }
